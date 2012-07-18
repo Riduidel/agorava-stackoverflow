ff79f295f72b89f02e5241c56d558ca818138185
Riduidel <nicolas.delsaux@gmail.com>
2012-07-18 14:40:29 +0200

    prepared project for some brute-force coding !

---
+++ agorava-stackoverflow-api/src/main/java/org/agorava/stackoverflow/StackOverflowUserService.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.agorava.stackoverflow;
+
+/**
+ * TODO borrow code from https://github.com/agorava/agorava-twitter/blob/develop/agorava-twitter-api/src/main/java/org/agorava/twitter/TwitterUserService.java
+ * @author ndx
+ */
+public interface StackOverflowUserService {
+    
+}
