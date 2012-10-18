2ea9e4614f74caf96f35ed11d11b4e9d3a3f3e93
Riduidel <nicolas.delsaux@gmail.com>
2012-10-18 10:56:41 +0200

    added (still empty for now) usage of the jsonschema2pojo plugin that should allow me to remove all implementations of beans from JSOn messages by replacing them with stock answers from StackExchange. Also fixed some strange dependencies versions bugs, mainly due (to my mind) to the fact that agorava dependencies are using a core.version property which value is 0.0.1.

--- agorava-stackoverflow-api/src/main/java/org/agorava/stackexchange/StackExchangeRevisionsService.java
+++ agorava-stackoverflow-api/src/main/java/org/agorava/stackexchange/StackExchangeRevisionsService.java
@@ -16,4 +16,3 @@ public interface StackExchangeRevisionsService extends StackExchangeURIs {
     public RevisionsWrapper getRevisions(StackExchangeQueryParameters<Date> query, String... ids);
 
 }
-X
